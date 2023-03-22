a69dfe895004e6e3b4607e57c65d36c48f7bd897
Aaron Ayerdis <aaronayerdis89@gmail.com>
2023-03-22 19:56:37 +0000

    reformatted db.py

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -57,15 +57,6 @@ class Db:
           return returning_id
     except Exception as err:
       self.print_sql_err(err)
-  # when we want to return a a single value
-  def query_value(self,sql,params={}):
-    self.print_sql('value',sql,params)
-
-    with self.pool.connection() as conn:
-      with conn.cursor() as cur:
-        cur.execute(sql,params)
-        json = cur.fetchone()
-        return json[0]
   # when we want to return a json object
   def query_array_json(self,sql,params={}):
     self.print_sql('array',sql,params)
@@ -78,6 +69,7 @@ class Db:
         return json[0]
   # When we want to return an array of json objects
   def query_object_json(self,sql,params={}):
+
     self.print_sql('json',sql,params)
     self.print_params(params)
     wrapped_sql = self.query_wrap_object(sql)
@@ -90,6 +82,13 @@ class Db:
           "{}"
         else:
           return json[0]
+  def query_value(self,sql,params={}):
+    self.print_sql('value',sql,params)
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(sql,params)
+        json = cur.fetchone()
+        return json[0]
   def query_wrap_object(self,template):
     sql = f"""
     (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
@@ -105,9 +104,18 @@ class Db:
     """
     return sql
   def print_sql_err(self,err):
-    if hasattr(err, 'pgerror'):
-        print("pgerror:", err.pgerror)
-    else:
-        print("An unknown error occurred:", err)
+    # get details about the exception
+    err_type, err_obj, traceback = sys.exc_info()
+
+    # get the line number when exception occured
+    line_num = traceback.tb_lineno
+
+    # print the connect() error
+    print ("\npsycopg ERROR:", err, "on line number:", line_num)
+    print ("psycopg traceback:", traceback, "-- type:", err_type)
+
+    # print the pgcode and pgerror exceptions
+    print ("pgerror:", err.pgerror)
+    print ("pgcode:", err.pgcode, "\n")
 
 db = Db()
